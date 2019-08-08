5446c3bf52eeeb31cee7e2ce6c22276bc04608d7
Callum Forbes <forbescallum@gmail.com>
2019-08-08 15:43:40 +0100

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,13 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    nums = [Kernel.rand(1..20), Kernel.rand(1..20), Kernel.rand(1..20)]
+    nums.map { |num| resp.write "#{num}\n" }
+    if nums.uniq.length == 1
+      resp.write "You Win!"
+    else
+      resp.write "You Lose."
+    end
     resp.finish
   end
 
